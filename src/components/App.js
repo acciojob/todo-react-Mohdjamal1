6cebcc9d42112cf33e0d1108ed17b01830c25d44
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-09-02 17:09:06 +0000

    new Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[text,setText] = useState("");
+  const[todos,setTodos] =useState([]);
+  function addTodo(){
+    if(text.trim() !== ""){
+      setTodos([...todos,text])
+      console.log(todos);
+      setText("");
+    }
+  }
+  function deleteTodo(id){
+    setTodos(todos.filter((item,index)=> index !== id));
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>To-Do List</h1>
+        <input value={text} onChange={(e)=>setText(e.target.value)} />
+        <button onClick={addTodo}>Add Todo</button>
+        <div>
+          {todos && todos.map((item,index)=>(
+            <div key={index}>{item}
+              <button onClick={()=>deleteTodo(index)}>Delete</button>
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
